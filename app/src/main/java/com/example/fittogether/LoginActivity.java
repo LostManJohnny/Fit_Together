ac9ccf564ff0475c4d8877759d0c8b7af4f30f9c
Matthew Major <mmajor2@academic.rrc.ca>
2022-09-16 08:45:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fittogether/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.fittogether;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+/*
+CoordinatorLayout - cdl
+ContraintLayout - csl
+AppBarLayout - abl
+Button - btn
+EditText - et
+TextView - tv
+ProgressBar - pb
+Checkbox - chk
+Radiobutton - rb
+ToggleButton - tb
+Spinner - spn
+Menu - mnu
+ */
