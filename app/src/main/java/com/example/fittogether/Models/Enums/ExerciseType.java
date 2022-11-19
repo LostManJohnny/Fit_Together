9d5ccbb28517aae6754c1b7b7bd4df1b92724f75
Matthew Major <mrmatias1997@gmail.com>
2022-11-19 02:52:55 -0600

    Added exercise type REP_TIME

--- app/src/main/java/com/example/fittogether/Models/Enums/ExerciseType.java
+++ app/src/main/java/com/example/fittogether/Models/Enums/ExerciseType.java
@@ -4,10 +4,12 @@ public enum ExerciseType {
     REP_WEIGHT, // Count reps and weight for each rep
     REP_ONLY, // Only count number of reps
     REP_BODY, // Only count number of reps for body weight
+    REP_TIME,
     TIME_ONLY, // Timer only
     FAILURE, // Rep to failure
     DROP, // Drop set
     ONE_REP_MAX // One rep max
+    ;
 }
 
 //TODO: Consider creating RepType and SetType
