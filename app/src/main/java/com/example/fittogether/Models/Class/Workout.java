8fdadd81b5cfe3f88327a885f381fb61cacff901
Matthew Major <mrmatias1997@gmail.com>
2022-11-19 02:52:30 -0600

    Added string to exercise type function

--- app/src/main/java/com/example/fittogether/Models/Class/Workout.java
+++ app/src/main/java/com/example/fittogether/Models/Class/Workout.java
@@ -1,5 +1,8 @@
 package com.example.fittogether.Models.Class;
 
+import com.example.fittogether.Api.Exceptions.IllegalExerciseTypeException;
+import com.example.fittogether.Models.Enums.ExerciseType;
+
 import java.util.ArrayList;
 
 public class Workout {
@@ -24,5 +27,29 @@ public class Workout {
 
     public int size(){
         return exercises.size();
+    }
+
+    public static ExerciseType getExerciseType(String type)
+            throws IllegalExerciseTypeException {
+
+        switch(type){
+            case "REP_WEIGHT":
+                return ExerciseType.REP_WEIGHT;
+            case "REP_BODY":
+                return ExerciseType.REP_BODY;
+            case "REP_TIME":
+                return ExerciseType.REP_TIME;
+            case "TIME_ONLY":
+                return ExerciseType.TIME_ONLY;
+            case "REP_ONLY":
+                return ExerciseType.REP_ONLY;
+            case "FAILURE":
+                return ExerciseType.FAILURE;
+            case "DROP":
+                return ExerciseType.DROP;
+            case "ONE_REP_MAX":
+                return ExerciseType.ONE_REP_MAX;
+        }
+        throw new IllegalExerciseTypeException("No exercise type by name (" + type + ")");
     }
 }
